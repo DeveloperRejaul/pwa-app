6124fe75a2639c87dd570754f3cfa64c56d3869a
rezaul <rezaul_karim@coredevs.ltd>
2024-01-17 11:27:42 +0600

    add

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,39 @@
 const cashData = 'pwd-app';
 
 // cache resource file
-this.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cashData).then((cache) => {
-      cache.addAll([
-        "/static/js/bundle.js",
-        "/index.html",
-        "/"
-      ]);
-    }),
-  );
-});
+// this.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open(cashData).then((cache) => {
+//       cache.addAll([
+//         "/static/js/bundle.js",
+//         "/index.html",
+//         "/"
+//       ]);
+//     }),
+//   );
+// });
 
 // get cached resource file
-this.addEventListener('fetch', (event) => {
-    if(navigator.onLine) return
-    event.respondWith(
-        caches.match(event.request).then((result) => {
-        if (result) return result;
-        }),
-    );
-});
+// this.addEventListener('fetch', (event) => {
+//     if(navigator.onLine) return
+//     event.respondWith(
+//         caches.match(event.request).then((result) => {
+//         if (result) return result;
+//         }),
+//     );
+// });
+
+
+
+// cache data
+// self.addEventListener("install", (event)=> {})
+
+
+// clean old cache data 
+// self.addEventListener("activate",(event)=>{})
+
+
+// update cache by cache name
+// self.importScripts("cache.js")
+
+
