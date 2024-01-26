c70df8998a3a08c3d654b460e9fb7b207e32b67d
Arrafi Mahin <arrafi.mahin@gmail.com>
2024-01-27 00:48:14 +0600

    image and file upload added

--- src/fetuses/chat/Bubble.jsx
+++ src/fetuses/chat/Bubble.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import chatCorner1 from '../../asset/icon/chatCorner1.svg';
 import chatCorner2 from '../../asset/icon/chatCorner2.svg';
-const Bubble = ({ data }) => {
+import { DownloadedIcon } from '../../asset/icon';
+const Bubble = ({ data = {} }) => {
     if (data.user == 'self') {
         return (
             <div className="relative">
                 <div className='bg-primary px-3 font-manrope text-white w-fit max-w-[60vw] rounded-[10px] pt-3'>
-                    <p className=" text-sm font-medium  leading-5">{data.text || ""}</p>
+                    {
+                        Object.hasOwn(data, 'text') && <p className=" text-sm font-medium  leading-5">{data.text || ""}</p>
+                    }
+                    {
+                        Object.hasOwn(data, 'image') && <img className="" src={data.image} alt="" />
+                    }
+                    {
+                        Object.hasOwn(data, 'file') && <a href={data.file.url} className="flex gap-2 items-center">{data.file.name} <DownloadedIcon className='h-5 w-5' /></a>
+                    }
                     <p className="text-[10px] text-end py-1">{data.time || ""}</p>
                 </div>
                 <img className='absolute -bottom-[6px] -left-[6px]' src={chatCorner2} />
-            </div>
+            </div >
         );
     } else {
         return (
