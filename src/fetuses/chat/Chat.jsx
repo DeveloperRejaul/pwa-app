e250429f7a429f428b421df654b685ae539ec593
Arrafi Mahin <arrafi.mahin@gmail.com>
2024-01-26 16:49:11 +0600

    chat updated

--- src/fetuses/chat/Chat.jsx
+++ src/fetuses/chat/Chat.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Bubble from './Bubble';
 import Sender from './Sender';
 import Header from './Header';
 import profile from '../../asset/Images/profile_picture.jpg';
-
+const chat = [
+    { text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '1/26/2024, 2:09:56 PM', user: "self" },
+    { text: "Lorem ipsum dolor sit amet ", time: '1/26/2024, 2:09:56 PM', user: "user" },
+    { text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '1/26/2024, 2:09:56 PM', user: "self" },
+    { text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '1/26/2024, 2:09:56 PM', user: "self" },
+    { text: "Lorem ipsum dolor sit amet ", time: '1/26/2024, 2:09:56 PM', user: "user" },
+    { text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '1/26/2024, 2:09:56 PM', user: "self" },
+    { text: "Lorem ipsum dolor sit amet ", time: '1/26/2024, 2:09:56 PM', user: "user" },
+    { text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '1/26/2024, 2:09:56 PM', user: "self" },
+]
 const Chat = () => {
+    const [chats, setChats] = useState(chat);
+    const bottomRef = useRef();
+    const submitHandler = (data) => {
+        console.log(data.get("text"));
+        return setChats([...chats, { text: data.get("text"), time: data.get("time"), user: data.get('user') },]);
+    };
+    useEffect(() => {
+        bottomRef.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'end'
+        })
+    }, [chats]);
     return (
         <div className='px-5 h-screen relative w-screen'>
             <Header data={{ name: ' Plot Name - Advisory', status: 'Online', image: profile }} />
-            <div className="space-y-7 overflow-y-auto w-full px-2 h-[85vh] no-scrollbar ">
+            <div className="space-y-7 overflow-y-auto w-full px-2 h-[83vh] no-scrollbar mb-5">
                 <div className='bg-[#C8996333] w-[5.44rem] mx-auto rounded-full p-[6px] text-center text-headline text-[8px]'>
                     <p className="">21 Mar 2023</p>
                 </div>
-                <Bubble data={{ text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '11:30 PM', user: "self" }} />
-                <Bubble data={{ text: "Lorem ipsum dolor sit amet ", time: '11:30 PM', user: "user" }} />
-                <Bubble data={{ text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '11:30 PM', user: "self" }} />
-                <Bubble data={{ text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '11:30 PM', user: "self" }} />
-                <Bubble data={{ text: "Lorem ipsum dolor sit amet ", time: '11:30 PM', user: "user" }} />
-                <Bubble data={{ text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '11:30 PM', user: "self" }} />
-                <Bubble data={{ text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '11:30 PM', user: "self" }} />
-                <Bubble data={{ text: "Lorem ipsum dolor sit amet ", time: '11:30 PM', user: "user" }} />
-                <Bubble data={{ text: "Lorem ipsum dolor sit amet, con sec tetur adipiscing elit. ", time: '11:30 PM', user: "self" }} />
+                {
+                    chats.reverse().map((item, index) => <Bubble key={index} data={item} />)
+                }
+                <div ref={bottomRef} className=""></div>
             </div>
             <div className="bottom-0 ">
-                <Sender />
+                <Sender submit={submitHandler} />
             </div>
         </div>
     );
--- src/fetuses/chat/Sender.jsx
+++ src/fetuses/chat/Sender.jsx
@@ -2,13 +2,40 @@ import React from 'react';
 import camera from '../../asset/icon/camera.svg';
 import clip from '../../asset/icon/paper-clip.svg';
 import plane from '../../asset/icon/paper-plane.svg';
-const Sender = () => {
+const Sender = ({ submit }) => {
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log("here", e)
+        const form = document.getElementById('chatForm');
+        const data = new FormData()
+        const date = new Date();
+        const image = e.target[0].value;
+        const text = e.target[1].value;
+        const file = e.target[3].value;
+        if (text != '') data.append("text", text);
+        if (file != '') data.append("file", file);
+        if (image != '') data.append("image", image);
+        data.append('time', date.toLocaleString());
+        data.append("user", "self")
+
+
+        form.reset();
+        submit(data);
+
+    };
     return (
-        <form className='flex items-center gap-3 w-full'>
+        <form id='chatForm' onSubmit={submitHandler} className='flex items-center gap-3 w-full'>
             <div className="flex items-center w-full gap-3 border border-border py-4 px-3 rounded-xl">
-                <img src={camera} alt="" />
+
+                <label htmlFor="image">
+                    <img src={camera} alt="" />
+                </label>
+                <input className='hidden' id='image' type="file" accept='image/*' />
                 <input className='w-full outline-none' type="text" placeholder='Type Message' />
-                <img src={clip} alt="" />
+                <label htmlFor="file">
+                    <img className='' src={clip} alt="" />
+                </label>
+                <input className='hidden' id='file' type="file" />
             </div>
             <button type="submit">
                 <img src={plane} alt="" />
