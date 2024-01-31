810efda2be6d2d79c8414fe8581ecd6f5c76a35f
mizanurhasan0 <sajal596@gmail.com>
2024-01-31 23:49:28 +0600

    chart add

--- src/fetuses/home/charts/Chart.jsx
+++ src/fetuses/home/charts/Chart.jsx
@@ -30,8 +30,8 @@ export default function Chart() {
                 show: false
             },
             xaxis: {
-                type: 'datetime',
-                categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+                type: 'category',
+                categories: ["May", "Jun", "July", "Aug", "Sep", "Oct", "Nov"]
             },
             fill: {
                 colors: ['#C89963', '#ffffff'],
@@ -48,11 +48,11 @@ export default function Chart() {
             grid: {
                 show: false
             },
-            tooltip: {
-                x: {
-                    format: 'dd/MM/yy HH:mm'
-                },
-            },
+            // tooltip: {
+            //     x: {
+            //         format: 'dd/MM/yy HH:mm'
+            //     },
+            // },
         },
     }
 
--- src/fetuses/home/chat_open/ChatList.jsx
+++ src/fetuses/home/chat_open/ChatList.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import FakeDate from '../../../db/AllRequest.json';
 
 export default function ChatList() {
     return (
@@ -7,16 +8,16 @@ export default function ChatList() {
                 <p className="text-xl leading-8">Chat Opened</p>
                 <button className="text-base opacity-30" type="button">See all</button>
             </div>
-            <div className="space-y-12">
-                {[1, 2, 3, 4, 5, 6].map((i) => (
+            <div className="space-y-12 overflow-y-auto">
+                {FakeDate.map((d, i) => (
                     <div key={i} className="flex items-center justify-between">
                         <div className="flex items-center space-x-2">
-                            <img src="https://cdn1.iconfinder.com/data/icons/website-internet/48/website_-_male_user-512.png" alt=""
-                                className="w-12 h-12 rounded-full overflow-hidden object-contain border bg-gray border-gray" />
-                            <p className="font-medium">Lorem</p>
+                            <img src={d.img} alt=""
+                                className="w-12 h-12 rounded-full overflow-hidden object-contain bg-gray " />
+                            <p className="font-medium">{d.username}</p>
                         </div>
                         <p>{new Date().toLocaleDateString()}</p>
-                        <button className="bg-primary text-secondary rounded-lg h-8 w-[5.8rem]" type="button">Open Chat</button>
+                        <button className="bg-primary text-secondary rounded-lg h-8 w-[5.8rem] text-sm" type="button">Open Chat</button>
                     </div>
                 ))}
             </div>
