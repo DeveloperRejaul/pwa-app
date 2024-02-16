15b5844f9ac6da3f699014c215394d8534547eca
mizanurhasan0 <sajal596@gmail.com>
2024-02-16 19:48:12 +0600

    update table

--- src/components/dropdown/CusDropdown.jsx
+++ src/components/dropdown/CusDropdown.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ArrowBottom } from '../../asset/icon';
 
-export default function CusDropdown() {
+export default function CusDropdown({ DefaultVal = "Select" }) {
+    const refDropdown = useRef();
+    const [selectOpt, setSelectOpt] = useState(DefaultVal);
+
+    const getSugBar = () => {
+        const dpd = refDropdown.current.querySelector('.customStyleDropdown');
+        const check = dpd.classList.contains('hidden');
+        return { check, dpd };
+    }
+
+    const onOpen = () => {
+        const { check, dpd } = getSugBar();
+        if (check) dpd.classList.remove('hidden');
+        else dpd.classList.add('hidden');
+    };
+
+    useEffect(() => {
+        const outsideClick = (e) => {
+            if (refDropdown.current && !refDropdown.current.contains(e.target)) {
+                const { check, dpd } = getSugBar();
+                if (!check) dpd.classList.add('hidden');
+            }
+        }
+        document.addEventListener('mousedown', outsideClick);
+        return () => document.removeEventListener('keydown', outsideClick);
+    }, []);
+
+    const onSelect = (opt) => {
+        setSelectOpt(opt);
+        const { check, dpd } = getSugBar();
+        if (!check) dpd.classList.add('hidden');
+    }
     return (
-        <div className='flex justify-between items-center w-[330px] border border-border px-3 py-4 rounded-md'>
-            <span className='font-poppins text-headline'>Ongoing</span>
-            <ArrowBottom className='cursor-pointer' />
+        <div ref={refDropdown} className="relative border border-primary border-opacity-40 h-[4.19rem] rounded-[0.46rem]">
+            <button
+                onClick={onOpen}
+                className="capitalize w-full text-left px-[.96rem] py-1
+                flex items-center justify-between text-[1.187rem] h-full">
+                <p className="">{selectOpt}</p>
+                <ArrowBottom />
+            </button>
+            <div
+                className="customStyleDropdown hidden flex flex-col absolute bg-white w-full text-sm border border-primary border-opacity-50 border-t-0 shadow-md rounded-b-md"
+            >
+                {[1, 2, 3, 4, 5].map((d) => (
+                    <button
+                        key={d}
+                        className="text-left px-2 py-1 hover:bg-secondary hover:text-txtPrimary rounded-sm whitespace-nowrap"
+                        onClick={() => onSelect('select' + d)}>
+                        Options 1
+                    </button>
+                ))}
+            </div>
         </div>
     )
 }
+
--- src/fetuses/knowledgeBank/KnowlegeBank.jsx
+++ src/fetuses/knowledgeBank/KnowlegeBank.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TblKnowledgeBank from './TblKnowledgeBank';
 import CusDropdown from '../../components/dropdown/CusDropdown';
+import Button from '../../components/button/Button';
 
 export default function KnowlegeBank() {
     return (
@@ -29,7 +30,19 @@ export default function KnowlegeBank() {
                 </div>
             </div>
             <div>
-                <CusDropdown />
+                <h2 className="pt-12 pb-7 font-semibold text-[1.75rem]">Control Measure</h2>
+                <div className="flex justify-between items-center py-[1.13rem]">
+                    <div className="flex items-center space-x-4">
+                        <div className="w-32">
+                            <CusDropdown />
+                        </div>
+                        <p>Entries</p>
+                    </div>
+                    <div className="space-x-4">
+                        <Button text="Add Control Measures" className="h-[4.13rem]" />
+                        <button className="border border-primary text-primary text-[1.186rem] font-medium px-[0.88rem] py-[1.186rem] rounded-[.449rem]">Update Excel</button>
+                    </div>
+                </div>
                 <TblKnowledgeBank />
             </div>
         </div>
